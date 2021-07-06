a434adba467fe29c131093b01abd57bfc151edaf
JeongJunPark <77726206+JeongJunPark@users.noreply.github.com>
2021-07-06 12:05:58 +0900

    Add files via upload

---
+++ procject_celib/view/admin/admins_edit.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+    <!-- Breadcrumbs -->
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?=base_url()?>admin">Dashboard</a></li>
+                <li class="breadcrumb-item">Settings</li>
+                <li class="breadcrumb-item"><a href="<?=base_url()?>admins/settings/admins">Admins</a></li>
+                <li class="breadcrumb-item active">Edit Admin</li>
+            </ol>
+
+        <form method="POST" action="">
+            <input type="hidden" name="action" value="edit">
+            <div class="form-group row">
+              <label for="example-text-input" class="col-2 col-form-label">Username</label>
+              <div class="col-10">
+                <input class="form-control" type="text" readonly value="<?=$result->username?>" placeholder="" name="username">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="example-text-input" class="col-2 col-form-label">Password (leave blank to keep)</label>
+              <div class="col-10">
+                <input class="form-control" type="password" value="" placeholder="" name="password">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="example-text-input" class="col-2 col-form-label">Password Verify (leave blank to keep)</label>
+              <div class="col-10">
+                <input class="form-control" type="password" value="" placeholder="" name="password2">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="example-text-input" class="col-2 col-form-label">Email</label>
+              <div class="col-10">
+                <input class="form-control" type="email" value="<?=$result->email?>" placeholder="" name="email">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="example-text-input" class="col-2 col-form-label">Name</label>
+              <div class="col-10">
+                <input class="form-control" type="text" value="<?=$result->name?>" placeholder="" name="name">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="example-text-input" class="col-2 col-form-label">Admin Group</label>
+              <div class="col-10">
+                <select class="form-control" name="admin_group" required>
+                <?php
+                foreach ($admin_groups as $v) {
+                    if ($v["id"] == $result->admin_group) { $selected = "selected"; } else {$selected = "";}
+                  echo "<option value='".$v["id"]."' ".$selected.">".$v["name"]."</option>";
+                } ?>
+                </select>
+              </div>
+            </div> 
+            <div class="form-group row"> 
+              <div class="col-10">
+                <input type="submit" class="btn btn-primary">
+              </div>
+            </div> 
+            
+
+        </form>
---
+++ procject_celib/view/admin/list.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+            <div class="card mb-3">
+                <div class="card-header">
+                    <i class="fa fa-table"></i> 입금 리스트
+                </div>
+                <div class="card-block">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" width="100%" id="tradeTable" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>거래ID</th>
+                                    <th>계약ID</th>
+                                    <th>계좌</th>
+                                    <th>입금자</th>
+                                    <th>입주자</th> 
+                                    <th>거래내역</th> 
+                                    <th>거래시간</th> 
+                                    <th>종류</th> 
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div> 
+            </div>
